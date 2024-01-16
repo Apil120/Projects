56a4980fd4071fa68ce9c8635f506e9a2f2abf61
Apil Adhikari <104585945+Apil120@users.noreply.github.com>
2024-01-16 11:40:46 +0545

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,14 +1,22 @@
+#Importing the necessary Packages:
 import os
 import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
-os.environ["GOOGLE_API_KEY"] = "AIzaSyA5_a3BnFaFRxLb4CvIqP446NvYtNyyCwo"
+
+
+#Setting up API key and choosing a LLM
+os.environ["GOOGLE_API_KEY"] = "AI-..YourAPI"
 llm = ChatGoogleGenerativeAI(model="gemini-pro")
+
+
+#UI using Streamlit:
 st.title("Chat with Google Generative AI")
 query = st.text_input("Enter a term you'd like to learn about:")
 if query:
     result = llm.invoke(query)
     st.text("Chat History:")
     st.write(result.content)
+    #Saving the conversation to a .txt file.
     with open("chat_history.txt", "a") as file:
         file.write(f"User: {query}\n")
         file.write(f"AI: {result.content}\n\n")
